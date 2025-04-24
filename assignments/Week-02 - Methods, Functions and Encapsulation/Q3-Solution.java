392e1a49a0072421e7e413e584e5e08e0cdb03b6
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-04-24 08:54:38 +0530

    Q3-Solution.java

---
+++ assignments/Week-02 - Methods, Functions and Encapsulation/Q3-Solution.java
@@ -0,0 +1,32 @@
+/*
+Write a program to demonstrate the use of this keyword. Create a class Rectangle with attributes length and breadth.
+Write a constructor to initialize these values. Also, define a method area() to return the area of the rectangle.
+*/
+
+import java.util.*;
+public class W02_P3{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int breadth = sc.nextInt();
+class Rectangle {
+    int length, breadth;
+
+    // Constructor
+    Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    // Method to calculate area
+    int area() {
+        return length * breadth;
+    }
+}
+// Create an object of Rectangle and call the area method
+        Rectangle r = new Rectangle(length, breadth);
+        System.out.print(r.area());
+
+        sc.close();
+    }
+}
