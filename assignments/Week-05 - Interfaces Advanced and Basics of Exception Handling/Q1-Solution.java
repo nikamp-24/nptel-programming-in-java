6e604846c1ca4a77163906d8dc86f7c0666d75d6
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-04-27 10:32:44 +0530

    Create Q1-Solution.java

---
+++ assignments/Week-05 - Interfaces Advanced and Basics of Exception Handling/Q1-Solution.java
@@ -0,0 +1,28 @@
+/*
+An interface Number is defined in the following program.
+You have to declare a class A, which will implement the interface Number
+*/
+
+
+import java.util.Scanner;
+
+interface Number {
+    int findSqr(int i);  // Returns the square of n
+}
+class A implements Number {
+  	//Define a method to find the square of a number
+	 int i, square;
+	 public int findSqr(int i) {
+		 square=i*i;
+            return square;		 
+      }
+}
+public class W05_P1{ 
+        public static void main (String[] args){ 
+	      A a = new A();   //Create an object of class A
+           // Read a number from the keyboard
+           Scanner sc = new Scanner(System.in);  
+           int i = sc.nextInt();
+           System.out.print(a.findSqr(i)); 
+    } 
+}
