1821f88e58ad95a6976f7b34cf3036a39681247d
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-05-09 14:57:28 +0530

    Create Q4-Solution.java

---
+++ assignments/Week-09 - Swing Programming and Abstract Window Toolkit(AWT)/Q4-Solution.java
@@ -0,0 +1,26 @@
+/*
+Write a Java program that reads an integer and determines if it is a prime number.
+ A prime number is only divisible by 1 and itself.
+*/
+
+import java.util.Scanner;
+
+public class W08_P4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         int number = scanner.nextInt();
+boolean isPrime = true;
+if (number <= 1) {
+    isPrime = false;
+} else {
+    for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+}
+System.out.println(isPrime ? "Prime" : "Not Prime");
+scanner.close();
+    }
+}
