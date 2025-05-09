31e28b627b8a545cbe564f32e38433605d8a3eec
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-05-09 14:58:41 +0530

    Create Q5-Solution.java

---
+++ assignments/Week-09 - Swing Programming and Abstract Window Toolkit(AWT)/Q5-Solution.java
@@ -0,0 +1,43 @@
+/*
+Complete the code to develop a BASIC CALCULATOR that can perform operations like Addition, Subtraction, Multiplication and Division.
+*/
+
+
+import java.util.Scanner;
+public class Question95{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine(); // Read as string, e.g., 5+6
+// Declare and initialize the required variable(s)
+		int i=0;
+		int j=0;
+		double output=0;
+		// Split the input string into character array
+		char seq[] = input.toCharArray();
+		/*
+		Use some method to separate the two operands
+		and then perform the required operation.
+		*/
+		for(int a=0; a<seq.length; a++){
+			if(seq[a]=='+'){
+				i= Integer.parseInt(input.substring(0,a));
+				j= Integer.parseInt(input.substring(a+1,seq.length));
+				output = (double)i+j;
+			}else if(seq[a]=='-'){
+				i= Integer.parseInt(input.substring(0,a));
+				j= Integer.parseInt(input.substring(a+1,seq.length));
+				output = (double)i-j;
+			}else if(seq[a]=='/'){
+				i= Integer.parseInt(input.substring(0,a));
+				j= Integer.parseInt(input.substring(a+1,seq.length));
+				output = (double)i/j;
+			}else if(seq[a]=='*'){
+				i= Integer.parseInt(input.substring(0,a));
+				j= Integer.parseInt(input.substring(a+1,seq.length));
+				output = (double)i*j;
+			}
+		}
+		// Print the output as stated in the question
+		System.out.print(input+" = " + Math.round(output));
+}
+}
