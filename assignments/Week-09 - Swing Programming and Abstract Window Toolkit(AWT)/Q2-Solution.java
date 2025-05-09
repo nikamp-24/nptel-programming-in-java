a005fecd34b86773212cd6ba807bbcdb9dea3a87
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-05-09 15:01:23 +0530

    Update Q2-Solution.java

--- assignments/Week-09 - Swing Programming and Abstract Window Toolkit(AWT)/Q2-Solution.java
+++ assignments/Week-09 - Swing Programming and Abstract Window Toolkit(AWT)/Q2-Solution.java
@@ -1,22 +1,43 @@
 /*
-Write a Java program that reads an integer from the user and computes its factorial
-Assume the input is a non-negative integer.
+A program needs to be developed which can mirror reflect any 5 × 5 2D character array into its side-by-side reflection.
+Write suitable code to achieve this transformation as shown below:
 */
 
 
 import java.util.Scanner;
-
-public class W08_P2 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-int factorial = 1;
-for (int i = 1; i <= n; i++) {
-    factorial *= i;
-}
-System.out.println(factorial);
-scanner.close();
-    }
-}
-
-
+public class Question92{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+// Declaring 5x5 2D char array to store input
+		char original[][]= new char[5][5];
+		
+		// Declaring 5x5 2D char array to store reflection
+		char reflection[][]= new char[5][5];
+		
+		// Input 2D Array using Scanner Class
+		for(int line=0;line<5; line++){
+			String input = sc.nextLine();
+			char seq[] = input.toCharArray();
+			if(seq.length==5){
+				for(int i=0;i<5;i++){
+					original[line][i]=seq[i];
+				}
+			}
+		}
+		
+		// Performing the reflection operation
+		for(int i=0; i<5;i++){
+			for(int j=0; j<5;j++){		
+				reflection[i][j]=original[i][4-j];
+			}
+		}
+		
+		// Output the 2D Reflection Array
+		for(int i=0; i<5;i++){
+			for(int j=0; j<5;j++){		
+				System.out.print(reflection[i][j]);
+			}
+			System.out.println();
+		}
+} // The main() method ends here
+} // The main class end
