22a5ebc623133944a8837f9cb2cce765e12d598c
Pooja Pratap Nikam <146447841+nikamp-24@users.noreply.github.com>
2025-05-09 15:00:02 +0530

    Update Q1-Solution.java

--- assignments/Week-09 - Swing Programming and Abstract Window Toolkit(AWT)/Q1-Solution.java
+++ assignments/Week-09 - Swing Programming and Abstract Window Toolkit(AWT)/Q1-Solution.java
@@ -1,20 +1,55 @@
 /*
-Write a Java program that reads a positive integer from the user and computes the sum of its digits
+Write suitable code to develop a 2D Flip-Flop Array with dimension 5 × 5,
+which replaces all input elements with values 0 by 1 and 1 by 0.
 */
 
-
 import java.util.Scanner;
+public class Question91{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+// Declare the 5X5 2D array to store the input
+		char original[][]= new char[5][5];
+		
+		// Input 2D Array using Scanner Class and check data validity
+		for(int line=0;line<5; line++){
+			String input = sc.nextLine();
+			char seq[] = input.toCharArray();
+			if(seq.length==5){
+				for(int i=0;i<5;i++){
+					if(seq[i]=='0' || seq[i]=='1'){
+						original[line][i]=seq[i];
+						if(line==4 && i==4)
+							flipflop(original);
+					}
+					else{
+						System.out.print("Only 0 and 1 supported.");
+						break;
+					}
+				}
+			}else{
+				System.out.print("Invalid length");
+				break;
+			}
 
-public class W08_P1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
-        int number = scanner.nextInt();
-int sum = 0;
-while (number > 0) {
-    sum += number % 10;
-    number /= 10;
-}
-System.out.print(sum);
-scanner.close();
-    }
-}
+		}
+	}
+	static void flipflop(char[][] flip){
+		// Flip-Flop Operation
+		for(int i=0; i<5;i++){
+			for(int j=0; j<5;j++){		
+				if(flip[i][j]=='1')
+					flip[i][j]='0';
+				else
+					flip[i][j]='1';
+			}
+		}
+	
+		// Output the 2D FlipFlopped Array
+		for(int i=0; i<5;i++){
+			for(int j=0; j<5;j++){		
+				System.out.print(flip[i][j]);
+			}
+			System.out.println();
+		}
+} // The main() ends here
+} // The main class ends here
